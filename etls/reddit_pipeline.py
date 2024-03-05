711ec82522217e2d82b4e9b7b86864687327861a
manhzeff <phamducmanh6624@gmail.com>
2024-03-05 12:13:11 +0700

    update code

---
+++ etls/reddit_pipeline.py
@@ -0,0 +1,18 @@
+import sys
+
+import numpy as np
+import pandas as pd
+import praw
+from praw import Reddit
+
+
+def connect_reddit(client_id,client_secret,user_agent)->Reddit:
+    try:
+        reddit=praw.Reddit(client_id=client_id,
+                           client_secret=client_secret,
+                           user_agent=user_agent)
+        print("Connected to Reddit!")
+        return reddit
+    except Exception as e:
+        print(e)
+        sys.exit(1)
